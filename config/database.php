1366002c662f822e0b9ce0ab546f2d0d33a2b31e
Yassine <66925471+ymefath@users.noreply.github.com>
2024-12-11 13:37:42 +0100

    Update db.php

--- config/database.php
+++ config/database.php
@@ -29,10 +29,6 @@ return [
     |
     */
 
-    'options' => [
-    PDO::MYSQL_ATTR_SSL_CA => 'C:\Users\HP EliteBook 840 G6\OneDrive\Documents\GitHub\ca.pem',
-],
-
 
     'connections' => [
 
@@ -48,24 +44,23 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+    'driver' => 'mysql',
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'unix_socket' => env('DB_SOCKET', ''),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => env('DB_STRICT', true),
+    'engine' => null,
+    'options' => [
+        PDO::MYSQL_ATTR_SSL_CA => base_path('C:\Users\HP EliteBook 840 G6\OneDrive\Documents\GitHub\ca.pem'),
+    ],
+],
+
 
         'mariadb' => [
             'driver' => 'mariadb',
