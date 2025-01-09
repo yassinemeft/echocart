ae26743245b13339bee17d653ed9bbb014262109
Yassine <ymeftah777@gmail.com>
2025-01-09 11:28:58 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-use App\Http\Controllers\ProductController;
 
 Route::resource('products', ProductController::class);
 
