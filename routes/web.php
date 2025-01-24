9a8e3594d5c61b3b0da63dc7f86ea880e9452f10
Yassine <ymeftah777@gmail.com>
2025-01-24 19:36:55 +0100

    Refactor routes to streamline product resource and update home, login, and sign routes

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-Route::resource('products', ProductController::class);
-Route::resource('products', ProductController::class);
 
+Route::resource('products', ProductController::class);
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
 })->name('home');
 
-
-Route::get('/sign', function () {
-    return view('sign');
-})->name('sign');
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
 Route::view('/login', 'login')->name('login');
-Route::view('/sign-up', 'sign')->name('register');
+Route::view('/sign', 'sign')->name('sign');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
