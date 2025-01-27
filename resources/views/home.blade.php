67b368b9191e3444d1f4c53853d39dd5e2adf098
Yassine <ymeftah777@gmail.com>
2025-01-27 22:03:24 +0100

    Refactor home.blade.php for improved readability; adjust styles and formatting for better structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,87 +3,89 @@
 @section('title', 'Home')
 
 @section('styles')
-<style>
-    .typing::before {
-        content: '';
-        position: absolute;
-        top: 0;
-        right: 0;
-        width: 2px;
-        height: 100%;
-        background: transparent;
-        animation: cursorBlink 0.8s steps(3) infinite;
-    }
-
-    @keyframes cursorBlink {
-        0%,
-        75% {
-            opacity: 1;
+    <style>
+        .typing::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 2px;
+            height: 100%;
+            background: transparent;
+            animation: cursorBlink 0.8s steps(3) infinite;
         }
 
-        76%,
-        100% {
-            opacity: 0;
-        }
-    }
+        @keyframes cursorBlink {
+            0%,
+            75% {
+                opacity: 1;
+            }
 
-    .typing {
-        position: relative;
-    }
-
-    .typing h2 {
-        position: relative;
-        color: hsl(195, 4.90%, 32.20%);
-        letter-spacing: 5px;
-        font-size: 4rem;
-        overflow: hidden;
-        margin-bottom: 0;
-        animation: type 5s steps(11) infinite;
-    }
-
-    @keyframes type {
-        0%,
-        100% {
-            width: 0px;
+            76%,
+            100% {
+                opacity: 0;
+            }
         }
 
-        30%,
-        60% {
-            width: 394.09px;
-        }
-    }
-</style>
-
-@media(max-width: 330px) {
-    .typing h2 {
-        font-size: 3rem;
-        animation: type 5s steps(10) infinite;
-    }
-
-    @keyframes type {
-        0%,
-        100% {
-            width: 0px;
+        .typing {
+            position: relative;
         }
 
-        30%,
-        60% {
-            width: 305px;
+        .typing h2 {
+            position: relative;
+            color: hsl(195, 4.90%, 32.20%);
+            letter-spacing: 5px;
+            font-size: 4rem;
+            overflow: hidden;
+            margin-bottom: 0;
+            animation: type 5s steps(11) infinite;
         }
-    }
-}
-</style>
+
+        @keyframes type {
+            0%,
+            100% {
+                width: 0px;
+            }
+
+            30%,
+            60% {
+                width: 394.09px;
+            }
+        }
+
+        @media(max-width: 330px) {
+            .typing h2 {
+                font-size: 3rem;
+                animation: type 5s steps(10) infinite;
+            }
+
+            @keyframes type {
+                0%,
+                100% {
+                    width: 0px;
+                }
+
+                30%,
+                60% {
+                    width: 305px;
+                }
+            }
+        }
+    </style>
 @endsection
 
 @section('content')
-    <div id="hero-section" class="p-5 text-center bg-dark vh-100" style="background-image: url('{{ asset('images/hero.jpg') }}'); background-size: cover; background-position: center;">
+    <div id="hero-section" class="p-5 text-center bg-dark vh-100" 
+         style="background-image: url('{{ asset('images/hero.jpg') }}'); background-size: cover; background-position: center;">
         <div class="mask" style="height: 100%;">
             <div class="d-flex justify-content-center align-items-center h-100">
                 <div class="text-dark">
                     <h1 class="display-3 mb-3">Welcome to EchoCart</h1>
                     <h4 class="mb-4">Lorem ipsum dolor amet consectetur adipisicing elit. Quisquam, quod.</h4>
-                    <a data-mdb-ripple-init class="btn btn-outline-dark btn-lg m-2" href="https://www.youtube.com/watch?v=c9B4TPnak1A" role="button" rel="nofollow" target="_blank">Explore our Products</a>
-                    <a data-mdb-ripple-init class="btn btn-outline-dark btn-lg m-2" href="https://mdbootstrap.com/docs/standard/" target="_blank" role="button">Learn more</a>
+                    <a data-mdb-ripple-init class="btn btn-outline-dark btn-lg m-2" 
+                       href="https://www.youtube.com/watch?v=c9B4TPnak1A" role="button" rel="nofollow" target="_blank">Explore our Products</a>
+                    <a data-mdb-ripple-init class="btn btn-outline-dark btn-lg m-2" 
+                       href="https://mdbootstrap.com/docs/standard/" target="_blank" role="button">Learn more</a>
                 </div>
             </div>
         </div>
@@ -116,7 +118,8 @@
         <h2 class="display-3 bold">Why Choose EchoCart</h2>
         <div class="d-flex py-4">
             <div class="feature col border-2 p-4 rounded shadow">
-                <div class="feature-icon d-inline-flex align-items-center justify-content-center bg-success text-white rounded-circle fs-3 mb-3" style="width: 60px; height: 60px;">
+                <div class="feature-icon d-inline-flex align-items-center justify-content-center bg-success text-white rounded-circle fs-3 mb-3" 
+                     style="width: 60px; height: 60px;">
                     <i class="bi bi-truck"></i>
                 </div>
                 <h3 class="fs-5 text-body-emphasis">Fast and Reliable Delivery</h3>
@@ -126,7 +129,8 @@
             </div>
 
             <div class="feature col border-2 p-4 rounded shadow mx-4">
-                <div class="feature-icon d-inline-flex align-items-center justify-content-center bg-success text-white rounded-circle fs-3 mb-3" style="width: 60px; height: 60px;">
+                <div class="feature-icon d-inline-flex align-items-center justify-content-center bg-success text-white rounded-circle fs-3 mb-3" 
+                     style="width: 60px; height: 60px;">
                     <i class="bi bi-credit-card"></i>
                 </div>
                 <h3 class="fs-5 text-body-emphasis">Secure Payment Options</h3>
@@ -136,7 +140,8 @@
             </div>
 
             <div class="feature col border-2 p-4 rounded shadow">
-                <div class="feature-icon d-inline-flex align-items-center justify-content-center bg-success text-white rounded-circle fs-3 mb-3" style="width: 60px; height: 60px;">
+                <div class="feature-icon d-inline-flex align-items-center justify-content-center bg-success text-white rounded-circle fs-3 mb-3" 
+                     style="width: 60px; height: 60px;">
                     <i class="bi bi-headset"></i>
                 </div>
                 <h3 class="fs-5 text-body-emphasis">Customer Support</h3>
@@ -150,66 +155,65 @@
     <div class="container my-5">
         <h2 class="text-center display-3 bold">Explore by Categories</h2>
         <div class="d-flex flex-wrap g-4">
-        @php
-    $categories = [
-        'Arts & Crafts', 
-        'Automotive', 
-        'Baby Products', 
-        'Beauty',
-        'Personal Care', 
-        'Electronics', 
-        'Fashion', 
-        'Health & Household', 
-        'Home & Kitchen', 
-        'Office Supplies', 
-        'Sports', 
-        'Toys & Games', 
-        'Books', 
-        'Pet Supplies', 
-        'Garden', 
-        'Gaming Consoles',
-        'Crafts', 
-        'Auto', 
-        'Tech', 
-        'Home', 
-        'Office', 
-        'Fitness', 
-        'Games',
-        'Music', 
-        'Appliances', 
-        'Outdoor', 
-        'Baby Gear', 
-        'Food & Drink', 
-        'Jewelry', 
-        'Furniture', 
-        'Tools', 
-        'Travel', 
-        'Luggage', 
-        'Stationery', 
-        'Party Supplies', 
-        'Education',
-        'Musical Instruments',
-        'Arts & Collectibles',
-        'Home Decor',
-    ];
-@endphp
-
-            
+            @php
+                $categories = [
+                    'Arts & Crafts', 
+                    'Automotive', 
+                    'Baby Products', 
+                    'Beauty',
+                    'Personal Care', 
+                    'Electronics', 
+                    'Fashion', 
+                    'Health & Household', 
+                    'Home & Kitchen', 
+                    'Office Supplies', 
+                    'Sports', 
+                    'Toys & Games', 
+                    'Books', 
+                    'Pet Supplies', 
+                    'Garden', 
+                    'Gaming Consoles',
+                    'Crafts', 
+                    'Auto', 
+                    'Tech', 
+                    'Home', 
+                    'Office', 
+                    'Fitness', 
+                    'Games',
+                    'Music', 
+                    'Appliances', 
+                    'Outdoor', 
+                    'Baby Gear', 
+                    'Food & Drink', 
+                    'Jewelry', 
+                    'Furniture', 
+                    'Tools', 
+                    'Travel', 
+                    'Luggage', 
+                    'Stationery', 
+                    'Party Supplies', 
+                    'Education',
+                    'Musical Instruments',
+                    'Arts & Collectibles',
+                    'Home Decor',
+                ];
+            @endphp
 
             @foreach ($categories as $index => $category)
-                <div class="d-flex  {{ $index % 2 === 0 ? 'ms-md-0' : 'ms-md-2' }} mt-3">
+                <div class="d-flex {{ $index % 2 === 0 ? 'ms-md-0' : 'ms-md-2' }} mt-3">
                     <div class="card text-center shadow border-0 mx-1">
-                        <div class="card-body fs-4" >
+                        <div class="card-body fs-4">
                             {{ $category }}
                         </div>
                     </div>
                 </div>
             @endforeach
         </div>
-        <div class="row text-center mt-2">
+        <div class="row text-center row-cols-3 row-cols-md-3 mt-2">
+            <span class="col"></span>
             <button class="btn btn-secondary btn-lg" type="button">And Many More</button>
+            <span class="col"></span>
         </div>
-
     </div>
 
     <a href="#hero-section" class="btn btn-secondary btn-md position-fixed bottom-0 end-0 m-4" style="z-index: 1000;">
