3030fb1c89d4affbee05af66e1bad4007f884bf7
Yassine <ymeftah777@gmail.com>
2025-01-28 15:00:36 +0100

    Add contact form routes for displaying and submitting inquiries

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,58 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Auth;
 
-Route::resource('products', ProductController::class);
+// Products resource
+Route::resource('products', ProductController::class)->only([
+    'index', 'show', 'create', 'store', 'edit', 'update', 'destroy'
+]);
 
+// Home page
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
+// Home page
 Route::get('/home', function () {
     return view('home');
 })->name('home');
 
+// Login and sign up pages
 Route::view('/login', 'login')->name('login');
 Route::view('/sign', 'sign')->name('sign');
 
+// Payement page
 Route::get('/payement', function () {
     return view('auth/payement');
 })->name('payement');
 
+// Products page
 Route::get('/products', function () {
     return view('products');
 })->name('products');
 
+// Authentication routes
 Auth::routes();
 
+// Home page after login
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Contact form
+use App\Http\Controllers\ContactController;
+
+Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.form');
+Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact.submit');
 
