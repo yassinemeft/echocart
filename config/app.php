c0c621e03603aec9e607c8142bb4c658ac2e0af6
Yassine <ymeftah777@gmail.com>
2025-02-06 00:20:47 +0100

    Add documentation for autoloaded service providers in app configuration

--- config/app.php
+++ config/app.php
@@ -123,4 +123,15 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+
 ];
